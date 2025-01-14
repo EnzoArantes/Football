579bf324f7d3043291760e153373869128dc1b66
Zo-hack <enzoarantes2000@gmail.com>
2025-01-14 15:53:23 -0500

    Initial Commit

---
+++ Football/FootballApp.swift
@@ -0,0 +1,17 @@
+//
+//  FootballApp.swift
+//  Football
+//
+//  Created by Enzo Arantes on 1/14/25.
+//
+
+import SwiftUI
+
+@main
+struct FootballApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
